43b0120c7dcda7b9690c36a69d7ccd3de3c68fef
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-01-30 16:56:04 -0300

    Creating Services

--- src/controllers/playersController.ts
+++ src/controllers/playersController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { getPlayerService } from "../services/playersServices";
 
 export const getPlayers = async (req: Request, res: Response) => {
-    res.status(200).send({player : "messi" });
+    const data = await getPlayerService();
+    res.status(200).send(data);
 }
