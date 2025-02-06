70709e593b6e773d8acbcf91a103566725480ebe
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-02-06 13:27:37 -0300

    Creating cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors"
 
 function createApp() {
 
@@ -7,6 +8,7 @@ function createApp() {
 
     app.use(express.json());
     app.use("/api", router)
+    app.use(cors())
 
     return app;
 }
